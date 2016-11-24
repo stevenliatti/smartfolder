dd76e3aaf429e3a553af004f4c3ad139bbd2bf5e
steven.liatti <steven.liatti@etu.hesge.ch>
2016-11-24 21:07:04 +0100

    Premiers tests avec la lib stat.h et dirent.h Drôle de comportement, quand le programme find est lancé avec le dossier courant (.) il liste bien les fichiers uniquement, comme voulu. Si on teste avec le dossier parent (..) ou un sous-dossier test contenant des fichiers, ça ne marche pas, soit tout le monde est considéré fichier, soit personne. --> A demander à Hoerdt

---
+++ src/find/test_stat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char** argv) {
+	struct stat buffer;
+	stat("a.txt", &buffer);
+
+	printf("%d\n", buffer.st_uid);
+	printf("%d\n", buffer.st_gid);
+	printf("%d\n", buffer.st_size);
+	printf("%d\n", buffer.st_atime);
+	printf("%d\n", buffer.st_mtime);
+	printf("%d\n", buffer.st_ctime);
+
+	return EXIT_SUCCESS;
+}
