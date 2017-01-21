63c219e391043d2afd998f185b49dee1bfe9ebfb
constant <matthieu.constant@etu.hesge.ch>
2017-01-21 14:09:58 +0100

    Parser

--- src/parser.h
+++ src/parser.h
@@ -1,10 +1,11 @@
 #include "wrapper.h"
 #include "logger.h"
 #include "argument.h"
+#include "list_arg.h"
 
 #ifndef _PARSER_H_
 #define _PARSER_H_
 
-int parse_arg(int argc, char** argv);
+list_t* parse_arg(int argc, char** argv);
 
 #endif
